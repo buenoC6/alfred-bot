6ef1766a9543e80e8defdced4b595c667068d750
Alexian Moins <alexian.moins@outlook.com>
2023-05-01 23:42:30 +0200

    node modules + fix to newer version

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-var token = process.env.TOKEN;
-const Discord = require('discord.js');
+const {
+    joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus, entersState,
+    NoSubscriberBehavior
+} = require('@discordjs/voice');
+var token = process.env.CLIENT_TOKEN;
 const fs = require('fs');
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates,
+    ]
+})
 let rawdata = fs.readFileSync('static/library.json')
 let myJson = JSON.parse(rawdata).library;
 
@@ -11,28 +23,32 @@ client.on("ready", function () {
 })
 
 client.on('message', message => {
-  for (var i = 0, len = myJson.length; i < len; i++) {
-    if (message.content === myJson[i].cmd ) {
-      var path = myJson[i].path+"";
-      const channel = message.member.voice.channel;
-      channel.join()
-      .then(connection => {
-        const dispatcher = connection.play(path, { volume: 0.5 });;
-        dispatcher.on("end", end => {connection.disconnect});
-      })
-      .catch(console.error);
+    console.log('test');
+    for (var i = 0, len = myJson.length; i < len; i++) {
+        if (message.content === myJson[i].cmd) {
+            var path = myJson[i].path + "";
+            const channel = message.member.voice.channel;
+            channel.join()
+                .then(connection => {
+                    const dispatcher = connection.play(path, {volume: 0.5});
+                    ;
+                    dispatcher.on("end", end => {
+                        connection.disconnect
+                    });
+                })
+                .catch(console.error);
+        }
     }
-  }
 });
 
 client.on('message', msg => {
-  if (msg.content === 'Alfredo aide moi') {
-    var helpList = "";
-    for (var i = 0, len = myJson.length; i < len; i++) {
-      helpList+=myJson[i].cmd+"\n";
+    if (msg.content === 'Alfredo aide moi') {
+        var helpList = "";
+        for (var i = 0, len = myJson.length; i < len; i++) {
+            helpList += myJson[i].cmd + "\n";
+        }
+        msg.reply('Voici les services que je propose mon brave: \n' + helpList);
     }
-    msg.reply('Voici les services que je propose mon brave: \n'+helpList);
-  }
 });
 
 
@@ -44,35 +60,40 @@ var http = require('http');
 var path = require('path');
 var app = express();
 var server = http.Server(app);
-var socketIO = require('socket.io');
-var io = socketIO.listen(server);
+var io = require('socket.io')(server);
 
 app.set('port', 8080);
 app.use('/static', express.static(__dirname + '/static'));
 // Routing
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
-server.listen(8080, function() {
-  console.log('Starting server on port 8080');
+server.listen(8080, function () {
+    console.log('Starting server on port 8080');
 });
 
-io.on('connection', function(socket) {
-  console.log("Un Soundboarder s'est connecté.");
+io.on('connection', function (socket) {
+    console.log("Un Soundboarder s'est connecté.");
 
-  socket.on('sound', function(file) {
-    console.log(file+" a été activé");
-    client.channels.cache.get('445294788976050186').join()
-    .then(connection => {
-      const dispatcher = connection.play(file, { volume: 0.5 });;
-      dispatcher.on("end", end => {connection.disconnect});
-    })
-    .catch(console.error);
-  });
+    socket.on('sound', function (file) {
+        console.log(file + " a été activé");
+        const currentChannel = client.channels.cache.get('954088649690079292')
 
-  socket.on('test', function() {
-    client.channels.cache.get('566673551394865254').send('Hello world!');
-  });
+        const connection = joinVoiceChannel({
+            channelId: currentChannel.id,
+            guildId: currentChannel.guildId,
+            adapterCreator: currentChannel.guild.voiceAdapterCreator
+        });
 
+        const player = createAudioPlayer()
+        const resource = createAudioResource(file)
+        connection.subscribe(player)
+
+        player.play(resource)
+    });
+
+    socket.on('test', function () {
+        client.channels.cache.get('566673551394865254').send('Hello world!');
+    });
 });
